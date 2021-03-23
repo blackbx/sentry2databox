0563c89cdef5c962575e0c40696cb92fce304551
Patrick Clover <patrick@stampede.ai>
2021-03-23 01:33:51 +0000

    sync data

--- src/sentry.js
+++ src/sentry.js
@@ -1,18 +1,21 @@
 const request = require("request-promise-native");
 
-exports.getProjects = function() {
-  const url = "https://sentry.io/api/0/organizations/"+process.env.SENTRY_ORG+"/projects/";
+exports.getProjects = function () {
+  const url =
+    "https://sentry.io/api/0/organizations/" +
+    process.env.SENTRY_ORG +
+    "/projects/";
   const options = {
-    "url": url,
-    "headers": {
+    url: url,
+    headers: {
       "Content-Type": "application/json",
-      "Authorization": "Bearer " + process.env.SENTRY_TOKEN
-    }
+      Authorization: "Bearer " + process.env.SENTRY_TOKEN,
+    },
   };
   let projectMap = {};
 
   return request(options)
-    .then(function(body) {
+    .then(function (body) {
       const projects = JSON.parse(body);
 
       for (let i = 0; i < projects.length; i++) {
@@ -21,45 +24,106 @@ exports.getProjects = function() {
 
       return projectMap;
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.log("code:", error.statusCode);
       console.log("body:", error.response.body);
     });
-}
+};
 
-exports.getEvents = function(since) {
-  const url = "https://sentry.io/api/0/organizations/" + process.env.SENTRY_ORG + "/stats/?stat=received&resolution=1d&group=project";
+exports.getEvents = function (since) {
+  const url =
+    "https://sentry.io/api/0/organizations/" +
+    process.env.SENTRY_ORG +
+    "/stats/?stat=received&resolution=1d&group=project";
   const options = {
-    "url": url + "&since=" + since,
-    "headers": {
+    url: url + "&since=" + since,
+    headers: {
       "Content-Type": "application/json",
-      "Authorization": "Bearer " + process.env.SENTRY_TOKEN
-    }
+      Authorization: "Bearer " + process.env.SENTRY_TOKEN,
+    },
   };
 
   return request(options)
-    .then(function(body) {
+    .then(function (body) {
       return JSON.parse(body);
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.log("code:", error.statusCode);
       console.log("body:", error.response.body);
     });
-}
+};
 
-exports.getEventsPerProject = function(since) {
-  that = this;
-  return that.getProjects()
-    .then(function(projects) {
-      return that.getEvents(since)
-        .then(function(events) {
-          return [projects, events];
-        });
+exports.listProjectEvents = function (projectId) {
+  const url =
+    "https://sentry.io/api/0/projects/" +
+    process.env.SENTRY_ORG +
+    "/" +
+    projectId +
+    "/events";
+  const options = {
+    url: url,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + process.env.SENTRY_TOKEN,
+    },
+  };
+
+  return request(options)
+    .then(function (body) {
+      return JSON.parse(body);
     })
-    .then(function(response) {
+    .catch(function (error) {
+      console.log("code:", error.statusCode);
+      console.log("body:", error.response.body);
+    });
+};
+
+
+
+exports.getListEventsPerProject = function () {
+  return that.getProjects().then(function (projects) {
+    const prejectKeyArray = Object.keys(projects).map((projectId) => projectId);
+    return Promise.all(
+      projectKeyArray.map((projectId) => that.listProjectEvents(projectId))
+    ).then((response) => {
+      res = {};
+      const databoxArray = [];
+      prejectKeyArray.forEach((projectId, key) => {
+        const events = response[key];
+        const projectName = projects[projectId];
+        
+        databoxArray.push({
+          data: events.map(event => ({
+            date: event.dateCreated,
+            "event.type": event["event.type"],
+            title: event.title,
+            serial: event.tags.find(tag => tag.key === 'serial')?.value,
+            organization_id: event.tags.find(tag => tag.key === 'organization.id')?.value,
+          })),
+          date: date,
+          key: "errors",
+          attributes: {
+            project: projectName,
+          },
+        });
+      });
+    });
+  });
+};
+
+exports.getEventsPerProject = function (since) {
+  that = this;
+  return that
+    .getProjects()
+    .then(function (projects) {
+      return that.getEvents(since).then(function (events) {
+        return [projects, events];
+      });
+    })
+    .then(function (response) {
       const projects = response[0];
       const events = response[1];
-      console.log({events, projects});
+      console.log({ events, projects });
       // extract project name, date and events count and prepare structure
       // {
       //    "project name": {
@@ -69,7 +133,7 @@ exports.getEventsPerProject = function(since) {
       //    },
       //    "project name 2": {...}
       // }
-      res = {}
+      res = {};
       for (let projectId in events) {
         const projectName = projects[projectId];
         for (let i = 0; i < events[projectId].length; i++) {
@@ -77,11 +141,11 @@ exports.getEventsPerProject = function(since) {
             res[projectName] = {};
           }
 
-          const date = (new Date(events[projectId][i][0] * 1000)).toUTCString();
+          const date = new Date(events[projectId][i][0] * 1000).toUTCString();
           res[projectName][date] = events[projectId][i][1];
         }
       }
 
       return res;
     });
-}
+};
